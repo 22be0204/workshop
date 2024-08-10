7838799c1531c1c285b4260cbe7eb6e68cf10db1
22be0204 <rishikadeshmukh2708@gmail.com>
2024-08-10 15:28:02 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+
+
+<?php
+
+$connect = mysqli_connect("localhost","root","","project1");
+ if($connect)
+ {
+echo "Database Connected";
+ }
+ else{
+ 	echo "Not connected, contact administrator";
+ }
+?>
